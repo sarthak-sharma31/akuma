322c28fe197da6bacb7e0d8cee34bcf08a634420
Sarthak Sharma <sarthak312005@gmail.com>
2026-03-13 22:03:30 +0530

    cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,6 @@
 const corsOptions = {
   origin: [
-    process.env.FRONTEND_URL || "http://localhost:3000",
-    // Add your Vercel URL after deploying:
-    // "https://akumafits.vercel.app",
+    "*",
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "x-admin-password"],
